892d678aa63425ddde7a8427d7e5717cc3a87878
fiza Noor <fnoor034726@gmail.com>
2026-09-07 00:17:14 +0500

    feat: implement user creation endpoint and add user schema

--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from schemas.users import UserResponse, UserCreate
 
 app = FastAPI()
+
+users = []
 
 @app.get("/", response_class=HTMLResponse, include_in_schema=False)
 def read_root():
 
     return f"<h1>Hello world</h1>"
 
+
+@app.post("/api/v1/users", response_model=UserResponse, status_code=201)
+def create_user(user:UserCreate):
+    new_user = {
+        "id": 1,
+        "full_name": user.full_name,
+        "email": user.email,
+        "password": user.password
+    }
+    users.append(new_user)
+    return new_user
 
